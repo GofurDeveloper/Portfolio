504d5c82545d87e4cadc8965c359d8b76cb059aa
G'ofur <108719325+GofurDeveloper@users.noreply.github.com>
2023-05-02 18:38:44 +0500

    NoPage togirlandi

--- turan/src/pages/NoPage.js
+++ turan/src/pages/NoPage.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import "../css/NoPage.css"
 function NoPage() {
   return (
-    <div>NoPage</div>
+     <>
+       <div className='nopage'>
+        <div className='noPage_text'>
+        <h1 className='error'>ERROR</h1> <br />
+        <h1 className='not_found'>404 NOT FOUND</h1>
+        </div>
+      
+       </div>
+     </>
   )
 }
 
